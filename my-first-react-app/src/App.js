c37925a9810d93fc45e058f479013a47d757d2fa
Long <89914013+long10a10302@users.noreply.github.com>
2024-11-06 23:56:42 +0700

    create-project

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+      <p>This is my first React app.</p>
+    </div>
+  );
+}
+
+export default App;
